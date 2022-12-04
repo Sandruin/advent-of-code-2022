4f04ec98d30dfb55bb815a3858631eb0de2f680e
Sandruin <sebastian.lederer@yandex.com>
2022-12-04 18:12:38 +0100

    day01b done

--- day01b/src/main.rs
+++ day01b/src/main.rs
@@ -1,15 +1,17 @@
 fn main() {
     let input = include_str!("../input.txt");
 
-    let output = input
+    let mut cals = input
         .split("\n\n")
         .map(|elf| {
             elf.lines()
                 .map(|cal| cal.parse::<u32>().unwrap())
                 .sum::<u32>()
         })
-        .max()
-        .unwrap();
+        .collect::<Vec<u32>>();
+
+    cals.sort();
+    let output: u32 = cals.iter().rev().take(3).sum();
 
     println!("{}", output);
 }

158a30987649815a1efab9f0a40b1be0ac12df23
Sandruin <sebastian.lederer@yandex.com>
2022-12-04 19:10:33 +0100

    day02a done

---
+++ day02a/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let input = include_str!("../input.txt");
+
+    let output = input
+        .lines()
+        .map(|game| {
+            (
+                game.chars().nth(0).unwrap() as i16 - b'A' as i16,
+                game.chars().nth(2).unwrap() as i16 - b'X' as i16,
+            )
+        })
+        .map(|(a, b)| b + 1 + 3 * ((1 + b - a).rem_euclid(3)))
+        .sum::<i16>();
+
+    println!("{}", output);
+}

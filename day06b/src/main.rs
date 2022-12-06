0d1e17992d408f286e307448d67334c9f74eb2ae
Sandruin <sebastian.lederer@yandex.com>
2022-12-06 10:45:08 +0100

    day06b done

--- day06b/src/main.rs
+++ day06b/src/main.rs
@@ -3,19 +3,19 @@ use itertools::Itertools;
 fn main() {
     let input = include_str!("../input.txt");
 
-    let mut last_four_letters: [char; 4] = Default::default();
+    let mut last_four_letters: [char; 14] = Default::default();
 
     let mut index = 0;
 
     for (i, char) in input.chars().enumerate() {
         // push new character into array
-        last_four_letters[i % 4] = char;
+        last_four_letters[i % 14] = char;
         // skip check if we don't have 4 letters yet
-        if i < 4 {
+        if i < 14 {
             continue;
         };
         // check if array has only unique values
-        if last_four_letters.iter().unique().count() == 4 {
+        if last_four_letters.iter().unique().count() == 14 {
             index = i + 1;
             break;
         }
